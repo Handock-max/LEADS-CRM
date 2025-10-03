460f48bbb4891acfac8d2f9d25ff2f622b6ec313
Handock-max <martindetours98@gmail.com>
2025-10-03 18:08:26 +0000

    hi

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/LEADS-CRM/' : '/',
+  base: '/LEADS-CRM/',
   server: {
     host: "::",
     port: 8080,
